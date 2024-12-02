d13fb0aef854747d750b07b27bf2643410134ebd
Ian-Wanjira <ianwanjira4@gmail.com>
2024-12-02 02:03:57 -0600

    feat: Add user authentication check to RootLayout component
    - Imported  from  to handle navigation redirection.
    - Imported  from  to fetch the logged-in user.
    - Updated  to an  function to await the logged-in user data.
      - Added authentication check: Redirects to  if no user is logged in.
      - If logged in, renders the  component with the user data.
    
    These changes introduce a user authentication check in the RootLayout component, enhancing security and ensuring only authenticated users can access the main layout.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
+import { redirect } from 'next/navigation';
 
 import MobileNav from '@/components/MobileNav';
 import SideBar from '@/components/SideBar';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: 'Ian', lastName: 'Wanjira' };
-
+  const loggedIn = await getLoggedInUser();
+  if (!loggedIn) redirect('/sign-in');
   return (
     <main className="flex h-screen w-full font-inter">
       <SideBar user={loggedIn} />
